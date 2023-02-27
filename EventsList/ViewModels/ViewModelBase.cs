add45a7534d4805c4cc49526b9c2e7577906ebbe
Kornilov A.A <Mr.foxyn@yandex.ru>
2023-02-27 20:41:30 +0700

    Добавление проекта в репозиторий
    + Добавление шаблона проекта с фреймворком Avalonia UI;
    * Изменение файла README.md.

---
+++ EventsList/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsList.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
